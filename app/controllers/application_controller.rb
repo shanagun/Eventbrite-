1c804db1f9c53fc5b169a11cfe424e655e46df8a
Pak <andrei-pak@outlook.fr>
2019-11-06 01:03:27 +0100

    edit some views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,4 +5,5 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up) {|u| u.permit(:first_name, :last_name, :description, :email, :password, :password_confirmation)}
     devise_parameter_sanitizer.permit(:account_update) {|u| u.permit(:first_name, :last_name, :description, :email, :password, :password_confirmation, :current_password)}
   end
+
 end
